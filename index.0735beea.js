708f8ac05ad565e75f8ab0f2c9748c3dcf611f1a
SahaidakVladyslav <94925822+SahaidakVladyslav@users.noreply.github.com>
2023-01-16 19:50:57 +0000

    Deploying to gh-pages from @ SahaidakVladyslav/The-Fellowship-of-the-Ring@a2501a23a456b898b1f301c8b205d3d732444b3c 🚀

---
+++ index.0735beea.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$((function(){$("#back-top").hide(),$(window).scroll((function(){$(this).scrollTop()>500?$("#back-top").fadeIn():$("#back-top").fadeOut()})),$("#back-top a").click((function(){return $("body,html").animate({scrollTop:0},800),!1}))}))}));
+//# sourceMappingURL=index.0735beea.js.map
