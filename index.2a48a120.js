0b13889afc4accebd5e5b38ffa1f5d1beb25c2f3
SahaidakVladyslav <94925822+SahaidakVladyslav@users.noreply.github.com>
2023-01-16 17:48:34 +0000

    Deploying to gh-pages from @ SahaidakVladyslav/The-Fellowship-of-the-Ring@aece31ac6652fa0b7df4e79d282e9fcdfdaaec2f 🚀

---
+++ index.2a48a120.js
@@ -0,0 +1,2 @@
+jQuery(document).ready((function(){jQuery("a.scrollto").click((function(){return elementClick=jQuery(this).attr("href"),destination=jQuery(elementClick).offset().top,jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop:destination},700),!1}))}));
+//# sourceMappingURL=index.2a48a120.js.map
