6d4258bc1fc96ce103e9dbce279b5a7ddb523500
SahaidakVladyslav <94925822+SahaidakVladyslav@users.noreply.github.com>
2023-01-17 21:57:45 +0000

    Deploying to gh-pages from @ SahaidakVladyslav/The-Fellowship-of-the-Ring@c1a253082ea98344d01f5fb6fee40ba23f8b798a 🚀

---
+++ index.7b1c2f5a.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".mySwiper",{speed:700,centeredSlides:!0,autoplay:{delay:6e3,disableOnInteraction:!1},pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".custom-next",prevEl:".custom-prev"},loop:!0});
+//# sourceMappingURL=index.7b1c2f5a.js.map
--- index.ded8b4ab.js
+++
@@ -1,2 +0,0 @@
-var text="BarberShop",delay=100,elem=document.getElementById("result"),print_text=function(e,t,n){e.length>0&&(t.innerHTML+=e[0],setTimeout((function(){print_text(e.slice(1),t,n)}),n))};print_text(text,elem,delay);
-//# sourceMappingURL=index.ded8b4ab.js.map
