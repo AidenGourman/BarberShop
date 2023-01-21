e5a5e6ec876bef51148ac935a9711088b20d3cff
AidenGourman <romangryshko18@gmail.com>
2023-01-21 02:32:47 +0100

    Header Done

---
+++ images/background/BarberShop_files/index.1f276fb4.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $(function() {
+        $("#back-top").hide();
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 500) $("#back-top").fadeIn();
+            else $("#back-top").fadeOut();
+        });
+        $("#back-top a").click(function() {
+            $("body,html").animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+    });
+});
+
+//# sourceMappingURL=index.1f276fb4.js.map
---
+++ images/background/BarberShop_files/index.25139ead.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".mySwiper", {
+    speed: 700,
+    centeredSlides: true,
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: false
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    navigation: {
+        nextEl: ".custom-next",
+        prevEl: ".custom-prev"
+    },
+    loop: true
+});
+
+//# sourceMappingURL=index.25139ead.js.map
